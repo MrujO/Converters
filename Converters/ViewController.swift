81c8f885b2631fcdf93e9e5eda90f8d3fd0131bb
Murillo R. Araujo <murillo_rodrigues@icloud.com>
2018-06-12 17:04:38 +0100

    Initial Commit

---
+++ Converters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Converters
+//
+//  Created by Murillo R. Araujo on 12/06/2018.
+//  Copyright © 2018 Code In Capsule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
