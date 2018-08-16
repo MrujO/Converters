da55292bd7b22121b0bb53918a8470f10cef2794
Murillo R. Araujo <murillo_rodrigues@icloud.com>
2018-08-16 23:25:56 +0100

    Added  more new converters

--- Converters/ViewController.swift
+++ Converters/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbUnit: UILabel!
     @IBOutlet weak var tfValue: UITextField!
     @IBOutlet weak var btUnit1: UIButton!
     @IBOutlet weak var btUnit2: UIButton!
@@ -26,5 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+        case "Temperature":
+            lbUnit.text = "Weight"
+            btUnit1.setTitle("Kilogram", for: .normal)
+            btUnit2.setTitle("Pound", for: .normal)
+        case "Weight":
+            lbUnit.text = "Money"
+            btUnit1.setTitle("Real", for: .normal)
+            btUnit2.setTitle("Euro", for: .normal)
+        case "Money":
+            lbUnit.text = "Distance"
+            btUnit1.setTitle("Meter", for: .normal)
+            btUnit2.setTitle("Kilometer", for: .normal)
+        default:
+            lbUnit.text = "Temperature"
+            btUnit1.setTitle("Celsius", for: .normal)
+            btUnit2.setTitle("Fahrenheit", for: .normal)
+        }
+        
+        convert(nil)
+    }
+    
+    @IBAction func convert(_ sender: UIButton?) {
+    }
 }
